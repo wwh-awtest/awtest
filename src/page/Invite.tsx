2c5b8907e42b3e19225262dff046525615fc8a68
James Wong <winghojameswong@gmail.com>
2021-08-24 23:04:53 +0800

    Invite page navigation

--- src/page/Invite.tsx
+++ src/page/Invite.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { Button, TextField } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+
+interface InviteRequest {
+  name: string;
+  email: string;
+  confirmEmail: string;
+}
 
 export function Invite() {
-  return <div>welcome</div>;
+  const history = useHistory();
+  const { register, handleSubmit } = useForm<InviteRequest>();
+  const onSubmit = handleSubmit((request) => {
+    history.push("/success");
+  });
+  return (
+    <form onSubmit={onSubmit}>
+      <TextField label="Full name" {...register("name")} />
+      <TextField label="Email" {...register("email")} />
+      <TextField label="Confirm Email" {...register("confirmEmail")} />
+      <Button variant="contained" color="primary" type="submit">
+        Send
+      </Button>
+    </form>
+  );
 }
