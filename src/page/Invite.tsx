cb3ff44cdd0efe901e9024506f385a929abd0919
James Wong <winghojameswong@gmail.com>
2021-08-24 23:30:31 +0800

    Form validation

--- src/page/Invite.tsx
+++ src/page/Invite.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { Button, TextField } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
+import { InviteService } from "../service/InviteService";
 
 interface InviteRequest {
   name: string;
@@ -11,15 +12,53 @@ interface InviteRequest {
 
 export function Invite() {
   const history = useHistory();
-  const { register, handleSubmit } = useForm<InviteRequest>();
-  const onSubmit = handleSubmit((request) => {
-    history.push("/success");
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<InviteRequest>();
+  const onSubmit = handleSubmit(({ name, email }) => {
+    InviteService.requestInvite({ name, email }).then((_) => {
+      history.push("/success");
+    });
   });
+
   return (
     <form onSubmit={onSubmit}>
-      <TextField label="Full name" {...register("name")} />
-      <TextField label="Email" {...register("email")} />
-      <TextField label="Confirm Email" {...register("confirmEmail")} />
+      <TextField
+        label="Full name"
+        {...register("name", {
+          required: "Full name is required",
+          minLength: {
+            value: 3,
+            message: "Full name must be as least 3 characters",
+          },
+        })}
+        error={errors.name != null}
+        helperText={errors.name?.message}
+      />
+      <TextField
+        label="Email"
+        {...register("email", {
+          required: "Email is required",
+          pattern: {
+            value: /\S+@\S+\.\S+/,
+            message: "Entered value does not match email format",
+          },
+        })}
+        error={errors.email != null}
+        helperText={errors.email?.message}
+      />
+      <TextField
+        label="Confirm Email"
+        {...register("confirmEmail", {
+          required: "Confirm Email is required",
+          validate: (value) => value === watch("email") || "Email must match",
+        })}
+        error={errors.confirmEmail != null}
+        helperText={errors.confirmEmail?.message}
+      />
       <Button variant="contained" color="primary" type="submit">
         Send
       </Button>
