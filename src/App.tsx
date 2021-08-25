b5ecc6e6cc548e7a8d88468e550200c4d7bf1ea5
James Wong <winghojameswong@gmail.com>
2021-08-25 21:14:25 +0800

    Home page style

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,47 @@
 import React from "react";
 import { Welcome } from "./page/Welcome";
 import "./App.css";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Typography } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    height: "100vh",
-  },
-  header: {},
-  body: {
-    flex: 1,
-  },
-  footer: {},
-}));
+const useStyles = makeStyles((theme) => {
+  return {
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      height: "100vh",
+    },
+    header: {
+      borderBottom: `1px solid ${theme.palette.divider}`,
+      padding: "20px 80px",
+    },
+    body: {
+      flex: 1,
+    },
+    footer: {
+      padding: "20px 80px",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      borderTop: `1px solid ${theme.palette.divider}`,
+    },
+  };
+});
 
 function App() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <div className={classes.header}>BROCCOLI & CO.</div>
+      <div className={classes.header}>
+        <Typography variant="h2">BROCCOLI & CO.</Typography>
+      </div>
       <div className={classes.body}>
         <Welcome />
       </div>
       <div className={classes.footer}>
-        <div>Made with &hearts; in Melbourne.</div>
-        <div>@2021 Broccoli & Co. All rights reserved.</div>
+        <Typography>Made with &hearts; in Melbourne.</Typography>
+        <Typography variant="subtitle1">
+          @2021 Broccoli & Co. All rights reserved.
+        </Typography>
       </div>
     </div>
   );
