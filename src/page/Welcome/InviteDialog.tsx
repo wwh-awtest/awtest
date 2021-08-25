6faf37751c4bb587ba98991f46d68ca03badaef3
James Wong <winghojameswong@gmail.com>
2021-08-25 21:26:00 +0800

    InviteDialog style

--- src/page/Welcome/InviteDialog.tsx
+++ src/page/Welcome/InviteDialog.tsx
@@ -1,8 +1,28 @@
 import React, { useReducer } from "react";
 import { useForm } from "react-hook-form";
-import { Dialog, TextField } from "@material-ui/core";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  makeStyles,
+  TextField,
+} from "@material-ui/core";
 import { Button } from "../../component/Button";
 import { InviteService } from "../../service/InviteService";
+
+const useStyles = makeStyles({
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    //    padding: "60px 40px",
+    "& > *": {
+      marginBottom: 10,
+    },
+  },
+  submitButton: {
+    marginTop: 40,
+  },
+});
 
 interface InviteRequest {
   name: string;
@@ -14,7 +34,9 @@ interface Props {
   onClose: () => void;
   onSuccess: () => void;
 }
+
 export function InviteDialog({ onClose, onSuccess }: Props) {
+  const classes = useStyles();
   const [state, dispatch] = useReducer(inviteRequestReducer, {
     loading: false,
     errorMessage: null,
@@ -38,51 +60,56 @@ export function InviteDialog({ onClose, onSuccess }: Props) {
   });
 
   return (
-    <Dialog open onClose={onClose}>
-      <form onSubmit={onSubmit}>
-        <TextField
-          label="Full name"
-          {...register("name", {
-            required: "Full name is required",
-            minLength: {
-              value: 3,
-              message: "Full name must be as least 3 characters",
-            },
-          })}
-          error={errors.name != null}
-          helperText={errors.name?.message}
-        />
-        <TextField
-          label="Email"
-          {...register("email", {
-            required: "Email is required",
-            pattern: {
-              value: /\S+@\S+\.\S+/,
-              message: "Entered value does not match email format",
-            },
-          })}
-          error={errors.email != null}
-          helperText={errors.email?.message}
-        />
-        <TextField
-          label="Confirm Email"
-          {...register("confirmEmail", {
-            required: "Confirm Email is required",
-            validate: (value) => value === watch("email") || "Email must match",
-          })}
-          error={errors.confirmEmail != null}
-          helperText={errors.confirmEmail?.message}
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          type="submit"
-          loading={state.loading}
-        >
-          Send
-        </Button>
-        {state.errorMessage}
-      </form>
+    <Dialog open onClose={onClose} fullWidth>
+      <DialogTitle> Request an Invite</DialogTitle>
+      <DialogContent>
+        <form onSubmit={onSubmit} className={classes.form}>
+          <TextField
+            label="Full name"
+            {...register("name", {
+              required: "Full name is required",
+              minLength: {
+                value: 3,
+                message: "Full name must be as least 3 characters",
+              },
+            })}
+            error={errors.name != null}
+            helperText={errors.name?.message}
+          />
+          <TextField
+            label="Email"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Entered value does not match email format",
+              },
+            })}
+            error={errors.email != null}
+            helperText={errors.email?.message}
+          />
+          <TextField
+            label="Confirm Email"
+            {...register("confirmEmail", {
+              required: "Confirm Email is required",
+              validate: (value) =>
+                value === watch("email") || "Email must match",
+            })}
+            error={errors.confirmEmail != null}
+            helperText={errors.confirmEmail?.message}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            loading={state.loading}
+            className={classes.submitButton}
+          >
+            Send
+          </Button>
+          {state.errorMessage}
+        </form>
+      </DialogContent>
     </Dialog>
   );
 }
