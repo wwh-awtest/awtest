76a103e99bb6dae804b3878f70646e1c4c3e7dd7
James Wong <winghojameswong@gmail.com>
2021-08-25 21:30:22 +0800

    SuccessDialog style

--- src/page/Welcome/SuccessDialog.tsx
+++ src/page/Welcome/SuccessDialog.tsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { Button, Dialog, DialogActions } from "@material-ui/core";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
 
 interface Props {
   onClose: () => void;
 }
 export function SuccessDialog({ onClose }: Props) {
   return (
-    <Dialog open onClose={onClose}>
+    <Dialog open onClose={onClose} fullWidth>
+      <DialogTitle>All done!</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          You will be one of the first to experience Broccoli & Co. when we
+          launch.
+        </DialogContentText>
+      </DialogContent>
       <DialogActions>
         <Button variant="contained" color="primary" onClick={onClose}>
           OK
