739eb3373c49dd4aed09bf061759d6f5a0398186
James Wong <winghojameswong@gmail.com>
2021-08-25 21:17:07 +0800

    Welcome page style

--- src/page/Welcome/index.tsx
+++ src/page/Welcome/index.tsx
@@ -1,14 +1,38 @@
 import React from "react";
-import { Button } from "@material-ui/core";
+import { Button, makeStyles, Typography } from "@material-ui/core";
 import { InviteDialog } from "./InviteDialog";
 import { SuccessDialog } from "./SuccessDialog";
 
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+  },
+});
+
 export function Welcome() {
   const [dialog, setDialog] = React.useState<null | "invite" | "success">(null);
+  const classes = useStyles();
 
   return (
-    <div>
-      <Button onClick={() => setDialog("invite")}>Request an invite</Button>
+    <div className={classes.root}>
+      <Typography variant="h3">A better way</Typography>
+      <Typography variant="h3" gutterBottom>
+        to enjoy everyday
+      </Typography>
+      <Typography color="textSecondary" gutterBottom>
+        Be the first to know hen we launch
+      </Typography>
+      <Button
+        onClick={() => setDialog("invite")}
+        color="primary"
+        variant="contained"
+      >
+        Request an invite
+      </Button>
       {dialog === "invite" && (
         <InviteDialog
           onClose={() => setDialog(null)}
